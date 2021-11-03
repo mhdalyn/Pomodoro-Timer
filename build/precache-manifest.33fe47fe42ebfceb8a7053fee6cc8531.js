fca47ae6b4a99664323ce6eb71d3f12c3a88fdbc
mhdalyn <61716067+mhdalyn@users.noreply.github.com>
2021-11-03 14:25:47 -0400

    deploying functionality

---
+++ build/precache-manifest.33fe47fe42ebfceb8a7053fee6cc8531.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc6d9fadb12cb3a5dafa5a43c750be6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "908384508bd4c2d740fb",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "908384508bd4c2d740fb",
+    "url": "/static/js/main.a3a10d09.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
